e717bcce5f92f6949f11e3fe83ddb6519031e60b
jjshko10 <staslukyanenko1995@gmail.com>
2023-09-11 19:12:52 +0300

    fix: image url

--- src/components/common/CustomCheckbox/index.jsx
+++ src/components/common/CustomCheckbox/index.jsx
@@ -4,7 +4,7 @@ const CustomCheckBox = ({ children, checked, onClick }) => {
 	return (
 		<div className={styles.checkbox_wrapper}>
 			<span onClick={() => onClick(!checked)} className={`${checked ? styles.checked : styles.not_checked}`}>
-				<img src="/src/assets/check-icon.svg" alt="checked"/>
+				<img src="/assets/check-icon.svg" alt="checked"/>
 			</span>
 			{children && <p className={styles.label}>{children}</p>}
 		</div>
