41d887efe3aa7b1c409487325e4a9c41bdbc647c
jjshko10 <staslukyanenko1995@gmail.com>
2023-10-06 13:32:10 +0300

    add: favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,3 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </div>
   </React.StrictMode>,
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
