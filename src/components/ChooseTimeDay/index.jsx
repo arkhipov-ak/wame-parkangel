ab818a164e25df5446eff1bcfee5038572b776f7
jjshko10 <staslukyanenko1995@gmail.com>
2024-01-17 17:46:02 +0300

    add: geo suggestions

--- src/components/ChooseTimeDay/index.jsx
+++ src/components/ChooseTimeDay/index.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSnapshot } from "valtio";
 import { useDebounce } from "use-debounce";
+import axios from "axios";
+import Autosuggest from "react-autosuggest";
 
 import styles from "./ChooseTimeToday.module.css";
 import NavBar from "src/components/NavBar";
@@ -13,6 +15,61 @@ import Button from "src/components/common/Button";
 import { showErrorSnackbar } from "src/utils/showSnackBar";
 import RegionSelect from "src/components/common/RegionSelect";
 import { hideKeyboard } from "src/utils/functions";
+import { GEO_SUGGEST_API_KEY } from "src/utils/constants";
+
+const theme = {
+  container: {
+    position: "relative"
+  },
+  input: {
+    height: "50px",
+    width: "100%",
+    textAlign: "center",
+    fontFamily: "Montserrat",
+    fontSize: "16px",
+    fontWeight: 400,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "15px",
+    /* background: var(--background-secondary), */
+    /* color: var(--text-primary), */
+    boxShadow: "0px 3px 10px 0px rgba(0, 0, 0, 0.05)",
+    border: "none",
+    margin: "2.5% 0 5% 0",
+  },
+  inputFocused: {
+    outline: "none"
+  },
+  suggestionsContainer: {
+    display: "none"
+  },
+  suggestionsContainerOpen: {
+    display: "block",
+    position: "absolute",
+    top: 61,
+    width: "100%",
+    border: "1px solid #aaa",
+    backgroundColor: "#fff",
+    fontFamily: "Helvetica, sans-serif",
+    fontWeight: 300,
+    fontSize: "16px",
+    borderRadius: "15px",
+    zIndex: 2
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: "none"
+  },
+  suggestion: {
+    cursor: "pointer",
+    padding: "10px 20px"
+  },
+  suggestionHighlighted: {
+    backgroundColor: "#ddd",
+    borderRadius: "15px",
+  }
+};
 
 const ChooseTimeDay = ({ day }) => {
   const snap = useSnapshot(state);
@@ -23,9 +80,37 @@ const ChooseTimeDay = ({ day }) => {
   const [hoursCount, setHoursCount] = useState(1);
   const [addressCoords, setAddressCoords] = useState(null);
   const [address, setAddress] = useState("");
-  const [debounceAddressValue] = useDebounce(address, 1000);
+  const [debounceAddressValue] = useDebounce(address, 500);
+  const [suggestions, setSuggestions] = useState([]);
   const navigate = useNavigate();
 
+  const onChange = (event, { newValue }) => setAddress(newValue);
+
+  const inputProps = {
+    placeholder: "Введите адрес",
+    value: address,
+    onChange,
+    onKeyDown: hideKeyboard,
+  };
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    axios.get(
+      `https://suggest-maps.yandex.ru/v1/suggest?apikey=${GEO_SUGGEST_API_KEY}&text=${value}`
+    ).then(r => {
+      if (r.data.results) setSuggestions(r.data.results.map(item => item.title.text));
+      else {
+        setSuggestions([])
+        showErrorSnackbar({ message: "Что-то пошло не так" })
+      }
+    }).catch(() => showErrorSnackbar({ message: "Не удалось получить список подсказок" }))
+  };
+
+  const onSuggestionsClearRequested = () => setSuggestions([]);
+
+  const getSuggestionValue = suggestion => suggestion;
+
+  const renderSuggestion = suggestion => <div>{suggestion}</div>;
+  
   const onHandleRedirect = (link) => {
     let hoursEndTemp = +selectedHours + +hoursCount;
 
@@ -148,14 +233,24 @@ const ChooseTimeDay = ({ day }) => {
           <span className={styles.label}>Ваш регион</span>
           <RegionSelect activeRegion={activeRegion} setActiveRegion={setActiveRegion}/>
           <span className={styles.label}>Адрес</span>
-          <input
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+            className={styles.input_style}
+            theme={theme}
+          />
+          {/* <input
             value={address}
             onChange={e => setAddress(e.target.value)}
             className={styles.input_style}
             placeholder="Введите адрес"
             onKeyDown={hideKeyboard}
             type="text"
-          />
+          /> */}
           <button
             type="button"
             className={styles.btn_style}
