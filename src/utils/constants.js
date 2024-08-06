9ee84cca412b0c4683f930f387d65f08c1caf21d
Arkhipov <dev.arkhipov@gmail.com>
2024-08-06 20:21:25 +0500

    edit constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const telegramToken = "6431382458:AAFMuD0o3qpnnd-QOS9nJOz9Yn-TBbKnhRQ";
+export const telegramToken = "5329612644:AAEcBDF8RpTz8VgqDyXaa-e_ezJghaIUWdo";
 export const API_KEY = "cfb7ca98-9e16-49b6-9147-4daad6d34284";
 export const GEO_SUGGEST_API_KEY = "c554d4ec-e65f-4853-9a96-b0cc0fed1289";
 export const supportLink = "https://t.me/ParkAngel_feedback";
